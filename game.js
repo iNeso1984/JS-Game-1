da3aafd3ee26d8a64bd9ae6f77bbe19eaf52a344
Vanessa W <88523307+iNeso1984@users.noreply.github.com>
2022-05-19 16:01:54 -0400

    updated code to add winners feature and start

--- game.js
+++ game.js
@@ -1,9 +1,19 @@
-var character = document.getElementById("character");
-var block = document.getElementById("block");
-var lose = document.getElementById("lose");
-var tryAgain = document.getElementById("tryAgain");
-var score = document.getElementById("score");
-var hitElement = document.querySelector(".hits");
+let character = document.getElementById("character");
+let block = document.getElementById("block");
+let lose = document.getElementById("lose");
+let tryAgain = document.getElementById("tryAgain");
+let score = document.getElementById("score");
+let hitElement = document.querySelector(".hits");
+let start = document.getElementById("start");
+let starts = document.querySelector(".starts");
+let winner = document.querySelector(".winner");
+
+//start button
+function startGame() {
+  block.style.display = "block";
+  starts.classList.toggle("hide");
+  scoreKeeper();
+}
 
 //jump animation
 function jump() {
@@ -20,11 +30,11 @@ function jump() {
 
 //going to windows and getting the top property value of character.
 //parseInt just gives you the number without px.
-var checkDead = setInterval(function () {
-  var characterTop = parseInt(
+let checkDead = setInterval(function () {
+  let characterTop = parseInt(
     window.getComputedStyle(character).getPropertyValue("top")
   );
-  var blockLeft = parseInt(
+  let blockLeft = parseInt(
     window.getComputedStyle(block).getPropertyValue("left")
   );
 
@@ -37,30 +47,38 @@ var checkDead = setInterval(function () {
     tryAgain.style.display = "block";
     character.style.display = "none";
     score.style.display = "none";
+    start.style.display = "none";
   }
 }, 10);
 
-//score keeper
-var hits = 0;
+//score keeper--> nested function
+function scoreKeeper() {
+  let hits = 0;
 
-var addHit = function () {
-  hits++;
-  renderHits();
-};
+  let addHit = function () {
+    hits++;
 
-var renderHits = function () {
-  hitElement.innerHTML = hits;
-};
-//spacebar listener
-window.addEventListener("keydown", checkKeyPress, false);
+    //winner
+    if (hits === 30) {
+      winner.classList.toggle("winner");
+      block.style.display = "none";
+      score.classList.toggle("hide");
+      tryAgain.style.display = "block";
+    }
+    renderHits();
+  };
 
-function checkKeyPress(key) {
-  if (key.keyCode == "32") addHit();
-  jump();
+  let renderHits = function () {
+    hitElement.innerHTML = hits;
+  };
+  //spacebar event listener
+  window.addEventListener("keydown", checkKeyPress, false);
+
+  function checkKeyPress(key) {
+    if (key.keyCode == "32") addHit();
+    jump();
+  }
 }
-checkKeyPress();
-
-//reload
 
 function restart() {
   location.reload();
