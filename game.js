98d87a24431d690368bb55019dce3dfa3797edac
Vanessa W <88523307+iNeso1984@users.noreply.github.com>
2022-05-19 16:05:32 -0400

    Update game.js

--- game.js
+++ game.js
@@ -4,6 +4,16 @@ let lose = document.getElementById("lose");
 let tryAgain = document.getElementById("tryAgain");
 let score = document.getElementById("score");
 let hitElement = document.querySelector(".hits");
+let start = document.getElementById("start");
+let starts = document.querySelector(".starts");
+let winner = document.querySelector(".winner");
+
+//start button
+function startGame() {
+  block.style.display = "block";
+  starts.classList.toggle("hide");
+  scoreKeeper();
+}
 
 //jump animation
 function jump() {
@@ -37,30 +47,38 @@ let checkDead = setInterval(function () {
     tryAgain.style.display = "block";
     character.style.display = "none";
     score.style.display = "none";
-  } 
+    start.style.display = "none";
+  }
 }, 10);
 
-//score keeper
-let hits = 0;
+//score keeper--> nested function
+function scoreKeeper() {
+  let hits = 0;
 
-let addHit = function () {
-  hits++;
-  renderHits();
-};
+  let addHit = function () {
+    hits++;
 
-let renderHits = function () {
-  hitElement.innerHTML = hits;
-};
-//spacebar event listener
-window.addEventListener("keydown", checkKeyPress, false);
+    //winner
+    if (hits === 30) {
+      winner.classList.toggle("winner");
+      block.style.display = "none";
+      score.classList.toggle("hide");
+      tryAgain.style.display = "block";
+    }
+    renderHits();
+  };
 
-function checkKeyPress(key) {
-  if (key.keyCode == "32") addHit();
-  jump();
+  let renderHits = function () {
+    hitElement.innerHTML = hits;
+  };
+  //spacebar event listener
+  window.addEventListener("keydown", checkKeyPress, false);
+
+  function checkKeyPress(key) {
+    if (key.keyCode == "32") addHit();
+    jump();
+  }
 }
-checkKeyPress();
-
-//reload
 
 function restart() {
   location.reload();
